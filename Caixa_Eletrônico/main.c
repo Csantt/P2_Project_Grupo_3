55df3b5ef84a98c847b2979df9a34b2ed08ea0dc
Idyl Icaro dos Santos <45442467+idylicaro@users.noreply.github.com>
2019-05-09 20:38:42 -0300

    init biblioteca função

--- Caixa_Eletrônico/main.c
+++ Caixa_Eletrônico/main.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* Biblioteca criada para organizar organizar o progama */
+#include "funçao.h"
+
+
 int main()
 {
-    printf("Hello world!\n");
+    writeTeste();
     return 0;
 }
+
