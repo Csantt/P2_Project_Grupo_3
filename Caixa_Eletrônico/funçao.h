7f6e7cb9e1982ca9150be2c04e03f2466b73b0f3
Idyl Icaro dos Santos <45442467+idylicaro@users.noreply.github.com>
2019-05-09 21:53:41 -0300

    init
    inicio modelo exemplo pra inicio do desenvolvimento

---
+++ Caixa_Eletrônico/funçao.h
@@ -0,0 +1,11 @@
+#ifndef FUNÁAO_H_INCLUDED
+#define FUNÁAO_H_INCLUDED
+
+//#include <C:\Users\Icaro\Documents\GitHub\P2_Project_Grupo_3\Caixa_EletrÙnico/fuctions.c>
+
+void writeTeste(){
+    printf("123 TESTANDO");
+}
+
+
+#endif // FUNÁAO_H_INCLUDED
--- Caixa_Eletrônico/main.c
+++ Caixa_Eletrônico/main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "funçao.h"
 int main()
 {
-    printf("Hello world!\n");
+    writeTeste();
+
     return 0;
 }
