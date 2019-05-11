9ae27201224982640c7363aa7b08982a936a26b2
Idyl Icaro dos Santos <45442467+idylicaro@users.noreply.github.com>
2019-05-11 10:58:08 -0300

    inicio cpp ,v.00

---
+++ Caixa_Eletrônico/fuctions.h
@@ -0,0 +1,14 @@
+#ifndef FUCTIONS_H_INCLUDED
+#define FUCTIONS_H_INCLUDED
+
+    struct conta{
+        int agencia;
+        int operacao;
+        int numConta;
+        int senha;
+    };
+
+
+
+
+#endif // FUCTIONS_H_INCLUDED
---
+++ Caixa_Eletrônico/main.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "fuctions.h"
+
+int main()
+{
+    conta usuario[3];
+
+
+
+    return 0;
+}
